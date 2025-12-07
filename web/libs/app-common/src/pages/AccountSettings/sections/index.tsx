07e3708f37ae2a58259ddea8dbd19a6ff9b5ba57
SMAghazadeh <maghazadeh085@gmail.com>
2025-12-07 15:56:09 +0330

    comment Hotkeys and Email Preferences and Personal Access Token  in my account

--- web/libs/app-common/src/pages/AccountSettings/sections/index.tsx
+++ web/libs/app-common/src/pages/AccountSettings/sections/index.tsx
@@ -26,42 +26,42 @@ export const accountSettingsSections = (settings: AuthTokenSettings, permissions
       id: "personal-info",
       component: PersonalInfo,
     },
-    {
-      title: (
-        <div className="flex items-center gap-tight">
-          <span>Hotkeys</span>
-          <Badge variant="beta">Beta</Badge>
-        </div>
-      ),
-      id: "hotkeys",
-      component: HotkeysManager,
-      description: () =>
-        "Customize your keyboard shortcuts to speed up your workflow. Click on any hotkey below to assign a new key combination that works best for you.",
-    },
-    {
-      title: "Email Preferences",
-      id: "email-preferences",
-      component: EmailPreferences,
-    },
+    // {
+    //   title: (
+    //     <div className="flex items-center gap-tight">
+    //       <span>Hotkeys</span>
+    //       <Badge variant="beta">Beta</Badge>
+    //     </div>
+    //   ),
+    //   id: "hotkeys",
+    //   component: HotkeysManager,
+    //   description: () =>
+    //     "Customize your keyboard shortcuts to speed up your workflow. Click on any hotkey below to assign a new key combination that works best for you.",
+    // },
+    // {
+    //   title: "Email Preferences",
+    //   id: "email-preferences",
+    //   component: EmailPreferences,
+    // },
     {
       title: "Membership Info",
       id: "membership-info",
       component: MembershipInfo,
     },
-    settings.api_tokens_enabled &&
-      canCreateTokens &&
-      ff.isActive(ff.FF_AUTH_TOKENS) && {
-        title: "Personal Access Token",
-        id: "personal-access-token",
-        component: PersonalJWTToken,
-        description: PersonalAccessTokenDescription,
-      },
-    settings.legacy_api_tokens_enabled &&
-      canCreateTokens && {
-        title: ff.isActive(ff.FF_AUTH_TOKENS) ? "Legacy Token" : "Access Token",
-        id: "legacy-token",
-        component: PersonalAccessToken,
-        description: PersonalAccessTokenDescription,
-      },
+    // settings.api_tokens_enabled &&
+    //   canCreateTokens &&
+    //   ff.isActive(ff.FF_AUTH_TOKENS) && {
+    //     title: "Personal Access Token",
+    //     id: "personal-access-token",
+    //     component: PersonalJWTToken,
+    //     description: PersonalAccessTokenDescription,
+    //   },
+    // settings.legacy_api_tokens_enabled &&
+    //   canCreateTokens && {
+    //     title: ff.isActive(ff.FF_AUTH_TOKENS) ? "Legacy Token" : "Access Token",
+    //     id: "legacy-token",
+    //     component: PersonalAccessToken,
+    //     description: PersonalAccessTokenDescription,
+    //   },
   ].filter(Boolean) as SectionType[];
 };
