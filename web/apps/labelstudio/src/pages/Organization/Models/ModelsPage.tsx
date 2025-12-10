363f888230e4d826e0342f1e61874745cae942c5
SMAghazadeh <maghazadeh085@gmail.com>
2025-12-10 13:26:58 +0330

    internationalization

--- web/apps/labelstudio/src/pages/Organization/Models/ModelsPage.tsx
+++ web/apps/labelstudio/src/pages/Organization/Models/ModelsPage.tsx
@@ -4,9 +4,13 @@ import { cn } from "apps/labelstudio/src/utils/bem";
 import { Link } from "react-router-dom";
 import type { Page } from "../../types/Page";
 import { EmptyList } from "./@components/EmptyList";
+import { useTranslation } from "react-i18next";
+import i18n from "i18next";
 
 export const ModelsPage: Page = () => {
-  useUpdatePageTitle("Models");
+  const { t } = useTranslation();
+
+  useUpdatePageTitle(t("organization.models.title"));
 
   return (
     <div className={cn("prompter").toClassName()}>
@@ -15,15 +19,16 @@ export const ModelsPage: Page = () => {
   );
 };
 
-ModelsPage.title = () => "Models";
+ModelsPage.title = () => i18n.t("organization.models.title");
 ModelsPage.titleRaw = "Models";
 ModelsPage.path = "/models";
 
 ModelsPage.context = () => {
+  const { t } = i18n;
   return (
     <Space size="small">
       <Link to="/prompt/settings" className={buttonVariant({ size: "small" })}>
-        Create Model
+        {t("organization.models.create")}
       </Link>
     </Space>
   );
